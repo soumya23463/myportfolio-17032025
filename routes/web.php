0424b54919833282040c75160a1a29e8f6a07491
soumya kesarwani <soumyakesrwani70@gmail.com>
2025-03-17 17:15:43 +0530

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,45 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+//students
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/courses', function () {
+    return view('courses');
+});
+Route::get('/projects', function () {
+    return view('courses');
+});
+Route::get('/blogs', function () {
+    return view('courses');
+});
+
+Route::get('/contacts', function () {
+    return view('courses');
+});
+
+//students
+Route::get('/student/login', function () {
+    return view('courses');
+});
+Route::get('/student/register', functaction: ion () {
+    return view('courses');
+});
+//dashboard
+Route::get('/dashboard/', function () {
+    return view('courses');
+});
+
+Route::get('/dashboard/courses', function () {
+    return view('courses');
+});
+Route::get('/dashboard/projects', function () {
+    return view('courses');
+});
+Route::get('/dashboard/blogs', function () {
+    return view('courses');
+});
+Route::get('/dashboard/setting', function () {
+    return view('courses');
 });
